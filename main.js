a4d6f38705d08445b46b7e082dfd31779a667cfe
Andrius <andriuss0316@gmail.com>
2021-07-13 10:14:29 +0300

    20210713

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 // septinta uzduotis
 
+let numberr = 0;
+
+const minusND7 = document.querySelector("minus");
+const plusND7 = document.querySelector("plus");
+const numberND7 = document.querySelector(".number");
+const resetND7 = document.querySelector(".reset");
+
+function minusClick() {
+    numberrND7.innerText = number - 1;
+}
+
+function plusClick() {
+    numberrND7.innerText = number + 1;
+}
+
+function resetClick() {
+    numberrND7.innerText = 0;
+}
+
+minusND7.addEventListener('Click', minusClick); 
+plussND7.addEventListener('Click', plusClick); 
+resetND7.addEventListener('Click', resetClick); 
+
